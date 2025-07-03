b3ed9fc1d27a11bd8df83fdec33748b82084bba3
ashraf452 <ashraf452b@gmail.com>
2025-07-03 06:16:22 +0600

    unsolved

---
+++ Binary Search/H_Hamburgers.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    FAST_IO;
+
+int t=1;
+// cin>>t;
+while(t--)
+{
+    string s;
+    cin>>s;
+    vector<int> n(3),p(3);
+    for(int i=0;i<3;i++)
+    {
+        cin>>n[i];
+    }
+    for(int i=0;i<3;i++)
+    {
+        cin>>p[i];
+    }
+    int r;
+    cin>>r;
+
+}
+
+   return 0;
+}
